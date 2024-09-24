b8d0bc06715a31a97903e315cca15e83935653ba
Nuno Nebeker <nebeker@users.noreply.github.com>
2024-09-24 18:24:06 +0100

    Add CLI parser.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short = 'i', long = "input")]
+    input: std::path::PathBuf,
+    #[arg(short = 'o', long = "output")]
+    output: std::path::PathBuf,
+}
+
 fn main() {
+    let args = Cli::parse();
+
     println!("Hello, world!");
+    println!(
+        "input path: {:?}, output path: {:?}",
+        args.input, args.output
+    )
 }
